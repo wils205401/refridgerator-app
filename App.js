c46b8d2c8db597d9d6645d3411679a81be7b454e
Wilson <wilson.leung.2054@hotmail.com>
2023-03-26 19:11:56 +0100

    initial react native app setup

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  StyleSheet,
+  Dimensions,
+  Text,
+  View,
+  Alert,
+  Image,
+  SafeAreaView,
+  TouchableHighlight,
+  Platform,
+} from "react-native";
 
 export default function App() {
+  console.log(Dimensions.get("screen"));
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: "100%",
+          height: "30%",
+        }}
+      ></View>
+      <Text>Welcome to Leo</Text>
+      <Button
+        title="Click Me"
+        onPress={() =>
+          Alert.prompt("Hello", "How are you?", (text) => console.log(text))
+        }
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
